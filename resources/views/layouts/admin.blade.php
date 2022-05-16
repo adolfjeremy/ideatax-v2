3d58ef7859f2326734839ca056aa36763266dd98
adolfjeremy <adolf.jer@gmail.com>
2022-05-16 18:31:29 +0700

    adding logo on admin page

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="shortcut icon" href="/images/ideatax-logo.png" type="image/x-icon">
+    <link rel="shortcut icon" href="/favicon.png" type="image/x-icon">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="/assets/vendors/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="/assets/css/pages/admin.css">
