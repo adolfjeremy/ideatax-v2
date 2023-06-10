471d30e67897c1f7d375c743362badb3e78e0468
adolfjeremy <adolf.jer@gmail.com>
2023-06-10 09:03:33 +0700

    install google tag manager on main layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -21,6 +21,15 @@
 
         gtag('config', 'G-YJ1NB0YCV4');
     </script>
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5BMXPXX');
+    </script>
+    <!-- End Google Tag Manager -->
     <link rel="shortcut icon" href="/favicon.png" type="image/x-icon">
     @include('includes.styles')
     <link href="/assets/vendors/aos\dist/aos.css" rel="stylesheet">
@@ -28,6 +37,12 @@
     <title>@yield('title')</title>
 </head>
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5BMXPXX"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
     @include('includes.header')
     <main>
         @yield('content')
